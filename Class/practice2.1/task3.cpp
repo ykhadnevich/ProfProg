7d13c43973596cf7cd6a5be72e5a256d1625efb6
ykhadnevich <ykhadnevich@kse.org.ua>
2024-05-20 15:49:40 +0000

    add tasks

--- Class/practice2.1/task3.cpp
+++ Class/practice2.1/task3.cpp
@@ -3,42 +3,66 @@
 #include <unordered_map>
 #include <string>
 
-const std::string DATABASE_FILE = "user_stats.txt";
-const std::string SECRET_WORD = "bread";
+class UserStatsManager {
+private:
+    const std::string DATABASE_FILE = "user_stats.txt";
+    const std::string SECRET_WORD = "bread";
+    std::unordered_map<std::string, int> userStats;
 
-void loadDatabase(std::unordered_map<std::string, int>& userStats) {
-    std::ifstream infile(DATABASE_FILE);
-    if (!infile.is_open()) return;
+    void loadDatabase() {
+        std::ifstream infile(DATABASE_FILE);
+        if (!infile.is_open()) return;
 
-    std::string name;
-    int count;
-    while (infile >> name >> count) {
-        userStats[name] = count;
+        std::string name;
+        int count;
+        while (infile >> name >> count) {
+            userStats[name] = count;
+        }
+
+        infile.close();
     }
 
-    infile.close();
-}
+    void saveDatabase() const {
+        std::ofstream outfile(DATABASE_FILE);
+        for (const auto& entry : userStats) {
+            outfile << entry.first << " " << entry.second << std::endl;
+        }
 
-void saveDatabase(const std::unordered_map<std::string, int>& userStats) {
-    std::ofstream outfile(DATABASE_FILE);
-    for (const auto& entry : userStats) {
-        outfile << entry.first << " " << entry.second << std::endl;
+        outfile.close();
     }
 
-    outfile.close();
-}
+public:
+    UserStatsManager() {
+        loadDatabase();
+    }
 
-void resetUser(const std::string& name, std::unordered_map<std::string, int>& userStats) {
-    userStats.erase(name);
-    std::cout << "Statistics for " << name << " have been reset." << std::endl;
-    saveDatabase(userStats);
-}
+    void resetUser(const std::string& name) {
+        userStats.erase(name);
+        std::cout << "Statistics for " << name << " have been reset." << std::endl;
+        saveDatabase();
+    }
 
-void exterminateAll(std::unordered_map<std::string, int>& userStats) {
-    userStats.clear();
-    std::cout << "All history exterminated." << std::endl;
-    saveDatabase(userStats);
-}
+    void exterminateAll() {
+        userStats.clear();
+        std::cout << "All history exterminated." << std::endl;
+        saveDatabase();
+    }
+
+    void processUser(const std::string& name) {
+        if (name == SECRET_WORD) {
+            exterminateAll();
+        } else {
+            if (userStats.find(name) == userStats.end()) {
+                userStats[name] = 0;
+                std::cout << "Welcome, " << name << "!" << std::endl;
+            } else {
+                userStats[name]++;
+                std::cout << "Hello again(x" << userStats[name] << "), " << name << "!" << std::endl;
+            }
+            saveDatabase();
+        }
+    }
+};
 
 int main(int argc, char* argv[]) {
     if (argc < 2 || argc > 3) {
@@ -49,22 +73,11 @@ int main(int argc, char* argv[]) {
     std::string name = argv[1];
     bool deleteFlag = (argc == 3 && std::string(argv[2]) == "delete");
 
-    std::unordered_map<std::string, int> userStats;
-    loadDatabase(userStats);
-
-    if (name == SECRET_WORD) {
-        exterminateAll(userStats);
-    } else if (deleteFlag) {
-        resetUser(name, userStats);
+    UserStatsManager statsManager;
+    if (deleteFlag) {
+        statsManager.resetUser(name);
     } else {
-        if (userStats.find(name) == userStats.end()) {
-            userStats[name] = 0;
-            std::cout << "Welcome, " << name << "!" << std::endl;
-        } else {
-            userStats[name]++;
-            std::cout << "Hello again(x" << userStats[name] << "), " << name << "!" << std::endl;
-        }
-        saveDatabase(userStats);
+        statsManager.processUser(name);
     }
 
     return 0;
---
+++ Class/practice2.2/task2.2
@@ -0,0 +1,34 @@
+**TASK. Favorite colour program**
+
+***1. Read the following user story and requirement.***
+
+***User story***
+
+As a user I want to be able to convert text file with an information related to image 16x16 based on my favorite colour preferences: if we have a pixel of my favorite color then corresponding left neighbor pixel and top neighbor pixel should be changed to that color. This will allow me to have more beatiful pictures.
+
+
+***Requirement 1:***
+
+User should be able to provide input file name, favorite color info and the name of the output file.
+
+***Requirement 2:***
+
+Program should be able to check each data point of the input 16x16 format and if it matches provided favorite colour then neighbor left and neighbor top data points should be set to that color as well.
+
+***Requirement 3:***
+
+Program should validate the input data format and report 
+
+***Design notes***
+
+We are plan to use input format like this. Each pixel is stored as 3 integers separated by comma, each integer should belong to the range from 0 to 255. Each line contains 16 pixels separated by space. File should contain 16 lines.
+
+
+***2. Test cases***
+
+Write 5 different test cases to cover different scenarios for the user story and requirements from above. Automate them.
+
+***3. Implementation***
+
+Implement a C++ program based on the requirements from above. Use your automation to test it.
+
